9bac73dbc604acfa09d8779226bc2f65767b708d
Daniele Cruz <doc.tur17@uea.edu.br>
2025-06-03 17:19:46 -0400

    inicialização da atividade extra

---
+++ biclicletinha/bicicleta.java
@@ -0,0 +1,38 @@
+    package bicicletinha;
+    public class bicicleta {
+    private int velocidade;
+    private int marcha;
+
+    public bicicleta() {
+        this.velocidade = 0;
+        this.marcha = 0;
+    }
+
+    public void acelerar() {
+        this.velocidade++;
+        ajusteMarcha();
+    }
+
+    public void frear() {
+        if (this.velocidade > 0) {
+            this.velocidade--;
+            ajusteMarcha();
+        }
+    }
+
+    private void ajusteMarcha() {
+        if (velocidade == 0) {
+            this.marcha = 0;
+        } else if (velocidade <= 5) {
+            this.marcha = 1;
+        } else if (velocidade <= 10) {
+            this.marcha = 2;
+        } else {
+            this.marcha = 3;
+        }
+    }
+
+    public void imprimir() {
+        System.out.println("Velocidade: " + this.velocidade + " km/h | Marcha: " + this.marcha);
+    }
+}
